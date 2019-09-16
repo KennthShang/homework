da73e168dad5e88a89c6fb7030e6d7393ff62ac1
Kenneth Shang <Kenneth.Shang@foxmail.com>
2019-09-16 14:12:50 +0800

    from workstation

--- DataEmit.py
+++ DataEmit.py
@@ -4,43 +4,42 @@ import re
 def create_datapoint(params):
 
 
-	w0 = params[1]
-	w1 = params[2]
-	w2 = params[3]
+    w0 = float(params[1])
+    w1 = float(params[2])
+    w2 = float(params[3])
 
-	line = np.array[w1, w2 ,w0]
-	pos_num = params[5]
-	neg_num = params[6]
+    line = np.array([w1, w2 ,w0])
+    pos_num = int(params[5])
+    neg_num = int(params[6])
 
-	pos_cnt = 0
-	neg_cnt = 0
-	pos_point = []
-	neg_point = []
-	while(pos_cnt < pos_num or neg_cnt < neg_num):
-		point = np.random.random(2)
-		vec = np.append(point, 1)
-		val = np.sign(vec.dot(line))
-		
-		if val < 0 and neg_cnt < neg_num:
-			neg_point.append(point)
-			neg_cnt += 1
+    pos_cnt = 0
+    neg_cnt = 0
+    pos_point = []
+    neg_point = []
+    while(pos_cnt < pos_num or neg_cnt < neg_num):
+        point = np.round(np.random.random(2), 2)
+        vec = np.append(point, 1)
+        val = np.sign(vec.dot(line))
 
-		elif val > 0 and pos_cnt < pos_num:
-			pos_point.append(point)
-			pos_num += 1
+        if val < 0 and neg_cnt < neg_num:
+            neg_point.append(point)
+            neg_cnt += 1
 
-	pos_point = np.array(pos_point)
-	pos_label = np.zeros(pos_num)+1
-	pos = np.c_[pos_point, pos_label]
+        elif val > 0 and pos_cnt < pos_num:
+            pos_point.append(point)
+            pos_cnt += 1
 
-	neg_point = np.array(neg_point)
-	neg_label = np.zeros(neg_num)-1
-	neg = np.c_[neg_point, neg_label]
+    pos_point = np.array(pos_point)
+    pos_label = np.zeros(pos_num)+1
+    pos = np.c_[pos_point, pos_label]
 
-	train = np.concatenate((pos, neg), axis=0)
+    neg_point = np.array(neg_point)
+    neg_label = np.zeros(neg_num)-1
+    neg = np.c_[neg_point, neg_label]
 
-	np.savetxt("train.txt", train, delimiter=",", fmt='%d')
+    train = np.concatenate((pos, neg), axis=0)
 
+    np.savetxt("train.txt", train, delimiter=",", fmt='%.2f')
 
 
 
@@ -48,10 +47,7 @@ def create_datapoint(params):
 
 
 if __name__ == '__main__':
-	string = input("DataEmit")
-	params = re.split(r'[<,>]\s', string)
-	
-	create_datapoint(params)
-
-
-
+    string = input("DataEmit")
+    params = re.split(r'[<,>\s]', string)
+    
+    create_datapoint(params)
---
+++ PLA.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def pla(feature, label):
+    w = np.random.uniform(-1,1,size=3)
+
+    flag = 0
+    while(flag == 0):
+        flag = 1
+        for i in range(len(feature)):
+            vec = np.append(feature[i], 1)
+            y = np.sign(vec.dot(w))
+
+            if label[i] != y:
+                w = w + label[i]*vec
+                flag = 0
+    return w
+
+
+
+
+
+
+if __name__ == '__main__':
+    file_name = input("PLA ")
+    data = np.genfromtxt(file_name, delimiter=',')
+    feature = data[:, :-1] 
+    label = data[:, -1]
+    w = pla(feature, label)
+
+    x = np.arange(0, 1, 0.01)
+    y = (w[0] * x1 + w[2]) / (-w[1])
+    pos_idx = np.where(label == 1)
+    neg_idx = np.where(label == -1)
+
+    pos_data = data[pos_idx]
+    neg_data = data[neg_idx]
+    plt.scatter(pos_data[:, 0], pos_data[:, 1], color='red')
+    plt.scatter(neg_data[:, 0], neg_data[:, 1], color='blue')
+    plt.plot(x, y)
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.show()
