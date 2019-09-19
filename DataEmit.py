4a28b400fbf58df9b9087851995e6a5707450fc2
Kenneth Shang <Kenneth.Shang@foxmail.com>
2019-09-19 10:31:35 +0800

    from workstation

--- DataEmit.py
+++ DataEmit.py
@@ -1,23 +1,37 @@
+%matplotlib inline
 import numpy as np
 import re
 
 def create_datapoint(params):
-
-
-    w0 = float(params[1])
-    w1 = float(params[2])
-    w2 = float(params[3])
-
-    line = np.array([w1, w2 ,w0])
-    pos_num = int(params[5])
-    neg_num = int(params[6])
-
+    """
+    Input: 
+        params: <w0, w1, w2> pos_num neg_num
+    """
+    try:
+        # Read in params from input
+        w0 = float(params[1])
+        w1 = float(params[2])
+        w2 = float(params[3])
+        pos_num = int(params[5])
+        neg_num = int(params[6])
+    except:
+        print("Input with wrong format")
+        return
+    else:
+        # Initialize the line
+        line = np.array([w1, w2 ,w0])
+        
+    
+    
+    
+    
+    # Create dataset
     pos_cnt = 0
     neg_cnt = 0
     pos_point = []
     neg_point = []
     while(pos_cnt < pos_num or neg_cnt < neg_num):
-        point = np.round(np.random.random(2), 2)
+        point = np.round(np.random.random(2)*20-10, 2)
         vec = np.append(point, 1)
         val = np.sign(vec.dot(line))
 
@@ -28,7 +42,8 @@ def create_datapoint(params):
         elif val > 0 and pos_cnt < pos_num:
             pos_point.append(point)
             pos_cnt += 1
-
+    
+    # Add label to the dataset
     pos_point = np.array(pos_point)
     pos_label = np.zeros(pos_num)+1
     pos = np.c_[pos_point, pos_label]
@@ -36,10 +51,12 @@ def create_datapoint(params):
     neg_point = np.array(neg_point)
     neg_label = np.zeros(neg_num)-1
     neg = np.c_[neg_point, neg_label]
-
+    
     train = np.concatenate((pos, neg), axis=0)
-
+    
+    # Save the dataset
     np.savetxt("train.txt", train, delimiter=",", fmt='%.2f')
+    print("Create datapoint program finished")
 
 
 
@@ -51,3 +68,4 @@ if __name__ == '__main__':
     params = re.split(r'[<,>\s]', string)
     
     create_datapoint(params)
+    
--- PLA.py
+++ PLA.py
@@ -2,8 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def pla(feature, label):
+    """
+    params:
+        feature: (x, y) of the point
+        label: (+, -) of the point
+    output:
+        w: the line
+    """
+    
     w = np.random.uniform(-1,1,size=3)
 
+    # Training part
     flag = 0
     while(flag == 0):
         flag = 1
@@ -16,20 +25,16 @@ def pla(feature, label):
                 flag = 0
     return w
 
-
-
-
-
-
-if __name__ == '__main__':
-    file_name = input("PLA ")
-    data = np.genfromtxt(file_name, delimiter=',')
-    feature = data[:, :-1] 
+def plot(w, data):
+    """
+    params: 
+        w: params of line
+        data: dataset
+    """
     label = data[:, -1]
-    w = pla(feature, label)
-
-    x = np.arange(0, 1, 0.01)
-    y = (w[0] * x1 + w[2]) / (-w[1])
+    
+    x = np.arange(-10, 10, 0.01)
+    y = (w[0] * x + w[2]) / (-w[1])
     pos_idx = np.where(label == 1)
     neg_idx = np.where(label == -1)
 
@@ -38,8 +43,19 @@ if __name__ == '__main__':
     plt.scatter(pos_data[:, 0], pos_data[:, 1], color='red')
     plt.scatter(neg_data[:, 0], neg_data[:, 1], color='blue')
     plt.plot(x, y)
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
     plt.show()
 
     print("PLA result: <" + str(w[2]) + "," + str(w[0]) + "," + str(w[1]) + ">")
+
+if __name__ == '__main__':
+    file_name = input("PLA ")
+    data = np.genfromtxt(file_name, delimiter=',')
+    feature = data[:, :-1] 
+    label = data[:, -1]
+    w = pla(feature, label)
+    
+    plot(w, data)
+    
+    
